d2167a52d3a186759c96c75939c07869607d8fb8
VaubanParty <yassine.badache@gmail.com>
2015-11-14 16:43:14 +0100

    Discoveries about filters, bug are filtered !

--- src/main/java/pfe/main/MainClass.java
+++ src/main/java/pfe/main/MainClass.java
@@ -1,19 +1,25 @@
 package pfe.main;
 
-import java.io.File;
 
-import fr.inria.sacha.spoon.diffSpoon.DiffSpoon;
+import org.gitective.core.CommitFinder;
+import org.gitective.core.filter.commit.AndCommitFilter;
+import org.gitective.core.filter.commit.CommitCountFilter;
+import org.gitective.core.filter.commit.CommitMessageFindFilter;
 
 
 
 public class MainClass {
 
 	public static void main (String args[])	throws Exception {
+		CommitCountFilter bugCommits = new CommitCountFilter();
+		CommitCountFilter testCommits = new CommitCountFilter();
+		CommitFinder finder = new CommitFinder("../gumtree-spoon-ast-diff/.git");
 		
-		File left = new File("sources/Left_test.java");
-		File right = new File("sources/Right_test.java");
+		// finder.setFilter(new AndCommitFilter(new CommitMessageFindFilter(":?(Test|test)"), testCommits));
+		finder.setFilter(new AndCommitFilter(new CommitMessageFindFilter(":?(Bug|bug)"), bugCommits));
+		finder.find();
 		
-		DiffSpoon dfsp = new DiffSpoon();
-		dfsp.compare(left, right);
+		// System.out.println(testCommits.getCount() + " total test commits");
+		System.out.println(bugCommits.getCount() + " total bug commits");
 	}
 }
